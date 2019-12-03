8e04642e1ea8e8d0b68fe32d922033ecf85e4d0f
Marko Dobric <marko.dobric@activecollab.com>
2019-12-03 20:55:19 +0100

    Tests controller

--- app/controllers/TestsController.php
+++ app/controllers/TestsController.php
@@ -1,7 +1,28 @@
 <?php
 
+use Kris\LaravelFormBuilder\FormBuilder;
 
-class TestsController
+class TestsController extends BaseController
 {
+    private $formBuilder;
 
+    public function __construct(FormBuilder $formBuilder)
+    {
+        $this->formBuilder = $formBuilder;
+    }
+
+    public function create()
+    {
+        $form = $this->formBuilder->create(TestForm::class, [
+            'method' => 'POST',
+            'url' => route('tests.store')
+        ]);
+
+        return View::make('tests.create', ['form' => $form]);
+    }
+
+    public function store()
+    {
+
+    }
 }
