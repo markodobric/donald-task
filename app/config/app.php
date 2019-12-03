c9337627cd77ec4cbf557a974a76b17ae663981e
Marko Dobric <marko.dobric@activecollab.com>
2019-12-03 14:25:18 +0100

    Prevent deprecation errors

--- app/config/app.php
+++ app/config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 return array(
 
 	/*
@@ -13,7 +15,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
